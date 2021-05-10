deba2a027252d1c9fdbfffc33dc2a550caa33d2f
alessio rea <alexei.80@hotmail.fr>
2021-05-10 17:18:31 +0200

    revert

--- actions/actions.py
+++ actions/actions.py
@@ -4,7 +4,8 @@ import sqlite3
 import pandas as pd
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
+from rasa_sdk.events import SlotSet, EventType
+import webbrowser
 
 
 class ActionFinalizeAppDetail(Action):
@@ -171,7 +172,55 @@ class ActionShowDeveloppers(Action):
 
 
 
+class ActionVideo(Action):
+    def name(self) -> Text:
+        return "action_video"
 
+    async def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        video_url="https://youtu.be/jj4BL9o3Q7o"
+        dispatcher.utter_message("wait... Playing your video.")
+        webbrowser.open(video_url)
+        return []
+
+
+
+class ValidateRestaurantForm(Action):
+    def name(self) -> Text:
+        return "user_details_form"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["age", "number"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                # The slot is not filled yet. Request the user to fill this slot next.
+                return [SlotSet("requested_slot", slot_name)]
+
+        # All slots are filled.
+        return [SlotSet("requested_slot", None)]
+
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_details_thanks",
+                                 age=tracker.get_slot("age"),
+                                 Mobile_number=tracker.get_slot("number"))
 
 
 
